62c376bc097d8af0258c818960287e6a4ad3ccc9
notarussianteenager <146776872+notarussianteenager@users.noreply.github.com>
2023-10-08 23:07:43 -0400

    Create __init__.py

---
+++ srf_attention/__init__.py
@@ -0,0 +1,6 @@
+from .rfs import simplex_random_matrix
+from .kernel import softmax_kernel_bhld, softmax_kernel_blhd
+from .att import Attention
+from .torch_low_mem_attn import low_mem_attn
+
+__version__ = '1.0.0'
